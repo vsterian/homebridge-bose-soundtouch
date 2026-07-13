ade10bc834bfce4d6c2ac9d79e47926b077915a8
Vlad Sterian <vlad.sterian@diconium.com>
2026-07-13 14:27:21 +0300

    Fix standalone plugin identity

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'BoseSoundTouch';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-bose-soundtouch';
+export const PLUGIN_NAME = 'homebridge-bose-soundtouch-tv-source';
